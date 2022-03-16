da8c3d79a23a09df3e3ebb4e98ea3a7f276f2103
Alen Toma <alen.toma@gmail.com>
2022-03-16 03:45:17 +0100

    ignore folder

--- dist/Database.d.ts
+++
@@ -1,4 +0,0 @@
-import { IDatabase } from './expo.sql.wrapper.types';
-import TablaStructor from './TableStructor';
-import * as SQLite from 'expo-sqlite';
-export default function <D extends string>(databaseTables: TablaStructor<any, D>[], getDatabase: () => Promise<SQLite.WebSQLDatabase>, onInit?: (database: IDatabase<D>) => Promise<void>): IDatabase<D>;
--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { IBaseModule, SingleValue, ArrayValue, StringValue, NumberValue, IQuery, IQueryResultItem, IDatabase, IWatcher } from './expo.sql.wrapper.types';
-import createDbContext from './Database';
-import TableStructor, { ColumnType } from './TableStructor';
-export default createDbContext;
-export { TableStructor, ColumnType, IBaseModule };
-export type { SingleValue, ArrayValue, NumberValue, StringValue, IQuery, IQueryResultItem, IDatabase, IWatcher };
